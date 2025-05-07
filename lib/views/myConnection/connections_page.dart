3198cfd3c3406108ead3a624ea6c44d19b9cb3f0
Harsh0726 <harshanineranjana@gmail.com>
2025-05-08 01:15:59 +0530

    created test pages for other tabs

---
+++ lib/views/myConnection/connections_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ConnectionsPage extends StatelessWidget {
+  const ConnectionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        
+            child:  Text('My Connections View'),
+
+        ),
+    );
+  }
+}

21f43feeae321fe79169c96bc6efab4787b80fba
Harsh0726 <harshanineranjana@gmail.com>
2025-05-08 18:54:34 +0530

    Created storu model dummy data

--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:linkedin_clone/models/post_model.dart';
+import 'package:linkedin_clone/models/story_model.dart';
+import 'package:linkedin_clone/utils/constants.dart';
 
 class HomeViewmodel extends ChangeNotifier {
   List<PostModel> _posts = [];
-  List<Map<String, dynamic>> _stories = [];
+  List<StoryModel> _stories = [];
 
   List<PostModel> get posts => _posts;
-  List<Map<String, dynamic>> get stories => _stories;
+  List<StoryModel> get stories => _stories;
 
   void fetchPosts() {
     _posts = [
       PostModel(
-        userName: 'Tony Antonio',
+        userName: 'Chandler Bing',
         content: 'Creating opportunity for every member of the global workforce drives everything we do at Link...',
         timeAgo: '1w • Edited',
         connectionLevel: '2nd',
         jobTitle: 'Android Dev at i Labs',
         likes: 'Harshani and 97 others',
         comments: '77',
+        userImage: Constants.storyImage3,
+        postImage: Constants.postImage1
       ),
       PostModel(
-        userName: 'Asa Butterfield',
+        userName: 'Monica Geller',
         content: 'Excited to announce that I have joined the team at i Labs as Flutter Intern!. Thank you to everyone who has supported me on this journey. I am looking forward to this new chapter in my career!',
         timeAgo: '2d • Edited',
         connectionLevel: '1st',
         jobTitle: 'Software Engineer at i Labs',
         likes: 'Tharushi and 50 others',
         comments: '20',
+        userImage: Constants.storyImage5,
+        postImage: Constants.postImage2
       ),
     ];
     notifyListeners();
@@ -34,12 +40,31 @@ class HomeViewmodel extends ChangeNotifier {
 
   void fetchStories() {
     _stories = [
-      {'name': 'Your Story', 'isUser': 'true'},
-      {'name': 'John Doe', 'isUser': 'false'},
-      {'name': 'Jane Smith', 'isUser': 'false'},
-      {'name': 'Alex Brown', 'isUser': 'false'},
-      {'name': 'Sara Wilson', 'isUser': 'false'},
-      {'name': 'Mike Lee', 'isUser': 'false'},
+      StoryModel(
+        userName: 'Asa Butterfield',
+        imageUrl: Constants.storyImage1,
+        isUser: true,
+      ),
+      StoryModel(
+        userName: 'Mave',
+        imageUrl: Constants.storyImage2,
+      ),
+      StoryModel(
+        userName: 'Chandler Bing',
+        imageUrl: Constants.storyImage3,
+      ),
+      StoryModel(
+        userName: 'Rachel Green',
+        imageUrl: Constants.storyImage4,
+      ),
+      StoryModel(
+        userName: 'Monica Geller',
+        imageUrl: Constants.storyImage5,
+      ),
+      StoryModel(
+        userName: 'Otis Milburn',
+        imageUrl: Constants.storyImage1,
+      ),
     ];
     notifyListeners();
   }

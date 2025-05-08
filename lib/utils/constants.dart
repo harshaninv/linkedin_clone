c7452a6948310886b098d1c4a32dde28bb607ed4
Harsh0726 <harshanineranjana@gmail.com>
2025-05-08 18:52:04 +0530

    Created image consants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+class Constants {
+  
+  static const String logoImage = 'assets/images/LI-Logo.png';
+  static const String profileImage1 = 'assets/images/asaprofile.png';
+  static const String profileImage2 = 'assets/images/maveprofile.png';
+  
+  static const String storyImage1 = 'assets/images/asaprofile.png';
+  static const String storyImage2 = 'assets/images/maveprofile.png';
+  static const String storyImage3 = 'assets/images/chandlerprofile.png';
+  static const String storyImage4 = 'assets/images/rachelprofile.png';
+  static const String storyImage5 = 'assets/images/monicaprofile.png';
+
+  static const String postImage1 = 'assets/images/post1.png';
+  static const String postImage2 = 'assets/images/post2.png';
+}// logo image from assets/images/logo.png
